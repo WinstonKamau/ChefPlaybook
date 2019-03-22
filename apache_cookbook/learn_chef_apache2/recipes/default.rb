e6c2df3ff1749f8bb42759f7d1328d3be2fa6917
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-03-22 15:43:33 +0300

    feat(user): add a web_admin user

--- apache_cookbook/learn_chef_apache2/recipes/default.rb
+++ apache_cookbook/learn_chef_apache2/recipes/default.rb
@@ -16,7 +16,18 @@ apt_update 'Update the apt cache daily' do
     action [:enable, :start]
   end
   
+  group 'web_admin'
+
+  user 'web_admin' do
+    group 'web_admin'
+    system true
+    shell '/bin/bash'
+  end
+
   template '/var/www/html/index.html' do
     source 'index.html.erb'
+    mode '0644'
+    owner 'web_admin'
+    group 'web_admin'
   end
   
